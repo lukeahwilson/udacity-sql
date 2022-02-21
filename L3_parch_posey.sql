5044338f7b7fcb38214e01634494480f1c3938ef
Luke Wilson <luke.a.h.wilson@gmail.com>
2022-02-21 14:54:17 -0800

    Completed lesson 3 dates questions

--- L3_parch_posey.sql
+++ L3_parch_posey.sql
@@ -65,14 +65,71 @@
 		-- LIMIT 20;
 
 -- Determine the number of times a particular channel was used in the web_events table for each sales rep.
-	SELECT s.name, w.channel, COUNT(*) AS channel_frequency
-	FROM web_events AS w
-	JOIN accounts AS a
-	ON a.id = w.account_id
-	JOIN sales_reps AS s
-	ON s.id = a.sales_rep_id
-	GROUP BY s.name, w.channel
-	ORDER BY s.name, channel_frequency
-	LIMIT 30;
+	-- SELECT s.name, w.channel, COUNT(*) AS channel_frequency
+	-- FROM web_events AS w
+	-- JOIN accounts AS a
+	-- ON a.id = w.account_id
+	-- JOIN sales_reps AS s
+	-- ON s.id = a.sales_rep_id
+	-- GROUP BY s.name, w.channel
+	-- ORDER BY s.name, channel_frequency
+	-- LIMIT 30;
 
---
+-- Test if there are any accounts associated with more than one region.
+	-- SELECT r.name, a.id, COUNT(*) AS num_accounts
+	-- FROM region AS r
+	-- JOIN sales_reps AS s
+	-- ON s.region_id = r.id
+	-- JOIN accounts AS a
+	-- ON a.sales_rep_id = s.id
+	-- GROUP BY r.name, a.id
+	-- ORDER BY num_accounts DESC
+	-- LIMIT 20;
+
+-- Have any sales reps worked on more than one account?
+	-- SELECT s.id, s.name, COUNT(*) num_accounts
+	-- FROM accounts a
+	-- JOIN sales_reps s
+	-- ON s.id = a.sales_rep_id
+	-- GROUP BY s.id, s.name
+	-- ORDER BY num_accounts;
+	--
+	-- SELECT COUNT(*)
+	-- FROM (SELECT DISTINCT id, name
+	-- 	FROM sales_reps) AS distinct_reps;
+
+-- Which accounts used facebook as a channel to contact customers more than 6 times?
+	-- SELECT a.name, w.channel, COUNT(*) AS chan_freq
+	-- FROM accounts AS a
+	-- JOIN web_events AS w
+	-- ON w.account_id = a.id
+	-- GROUP BY a.name, w.channel
+	-- HAVING w.channel = 'facebook' AND COUNT(*) > 6
+	-- ORDER BY chan_freq DESC
+	-- LIMIT 50;
+
+-- How many accounts spent less than 1,000 usd total across all orders?
+	-- SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+	-- FROM accounts a
+	-- JOIN orders o
+	-- ON a.id = o.account_id
+	-- GROUP BY a.id, a.name
+	-- HAVING SUM(o.total_amt_usd) < 1000
+	-- ORDER BY total_spent;
+
+-- Which month did Parch & Posey have the greatest sales in terms of total dollars?
+	-- SELECT DATE_PART('month', occurred_at) ord_month, SUM(total_amt_usd) total_spent
+	-- FROM orders
+	-- WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+	-- GROUP BY 1
+	-- ORDER BY 2 DESC;
+
+-- In which month of which year did Walmart spend the most on gloss paper in terms of dollars?
+	SELECT DATE_TRUNC('month', o.occurred_at) ord_date, SUM(o.gloss_amt_usd) tot_spent
+	FROM orders o
+	JOIN accounts a
+	ON a.id = o.account_id
+	WHERE a.name = 'Walmart'
+	GROUP BY 1
+	ORDER BY 2 DESC
+	LIMIT 5;
